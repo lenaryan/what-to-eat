d8de0e5de64d6aa8f7a347dcc4ef5cc2cf3ccd25
Lena Ryan <alissadyiphil@gmail.com>
2023-12-21 16:34:53 +0600

    added carousel

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,21 +2,23 @@ import { Link } from 'react-router-dom';
 import styles from './Main.module.css';
 import cn from 'classnames'
 import { urls } from '../../constants';
+import Carousel from '../../components/Carousel';
+
+const byingList = ["мюсли", "котлеты", "оливки", "лук", "помидоры", "попить"]
 
 const Main = () => {
     return (
         <section className="container relativeContainer">
             <h2>Меню</h2>
-            <p>карусель</p>
+            <Carousel />
             <button type="button" className={cn(`button ${styles.eatBtn}`)}>Что будем есть?</button>
             <h2>Список покупок</h2>
             <ul className="list">
-                <li className="list__item">мюсли</li>
-                <li className="list__item">котлеты</li>
-                <li className="list__item">оливки</li>
-                <li className="list__item">лук</li>
-                <li className="list__item">помидоры</li>
-                <li className="list__item">попить</li>
+                {
+                    byingList.map(product => (
+                        <li className="list__item" key={product}>{product}</li>
+                    ))
+                }
             </ul>
             <Link to={urls.ingredients} className="button fixedBtn">Что в холодильнике?</Link>
         </section>
